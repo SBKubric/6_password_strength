76a44b99563c8e4f13154b7b2f4bd94e976739ce
SBKubric <sbkubric@gmail.com>
2016-12-29 23:52:50 +0300

    All style mistakes were corrected.

--- password_strength.py
+++ password_strength.py
@@ -4,9 +4,6 @@ import urllib.request as request
 import argparse
 import getpass
 
-global blacklist_location
-global url_default
-
 
 def parse_args():
     parser = argparse.ArgumentParser(description='The script is designed for estimating strength of your password.')
@@ -18,56 +15,44 @@ def parse_args():
     return parser.parse_args()
 
 
-def set_blacklist(path, url, *, do_upload=False):
-    print('Checking if there is a local copy of the blacklist_file...')
-    if os.path.isfile(path) and not do_upload:
-        return None
-    print('Downloading the blacklist_file...')
-    request.urlretrieve(url, 'password_list')
+def upload_blacklist(path, url):
+    request.urlretrieve(url, path)
 
 
-def check_if_in_blacklist(password):
+def check_if_in_blacklist(password, blacklist_location):
     with open(blacklist_location) as password_list:
         blacklist = re.findall(r'\w+', password_list.read())
     if password.lower() in blacklist:
         return True
 
 
-def check_for_errors(password):
-    blacklist_error = check_if_in_blacklist(password)
-    len_error = len(password) < 6
-    long_check = len(password) >= 10
-    digit_error = re.search(r'[0-9]', password) is None
-    lowercase_error = re.search(r'[a-z]', password) is None
-    uppercase_error = re.search(r'[A-Z]', password) is None
-    special_error = re.search(r'\W', password) is None
-
+def check_for_errors(password, blacklist_location):
     return {
-        'blacklist_error': blacklist_error,
-        'len_error': len_error,
-        'long_check': long_check,
-        'digit_error': digit_error,
-        'lowercase_error': lowercase_error,
-        'uppercase_error': uppercase_error,
-        'special_error': special_error,
+        'blacklist_check': check_if_in_blacklist(password, blacklist_location),
+        'short_password_check': len(password) < 6,
+        'long_password_check': len(password) >= 10,
+        'digit_check': re.search(r'[0-9]', password) is not None,
+        'lowercase_check': re.search(r'[a-z]', password) is not None,
+        'uppercase_check': re.search(r'[A-Z]', password) is not None,
+        'special_check': re.search(r'\W', password) is not None,
     }
 
 
 def get_password_strength(check_results):
-    if check_results['blacklist_error']:
+    if check_results['blacklist_check']:
         return 1
-    if check_results['len_error']:
+    if check_results['short_password_check']:
         return 2
     sum = 3
-    if check_results['long_check']:
+    if check_results['long_password_check']:
         sum += 2
-    if not check_results['digit_error']:
+    if check_results['digit_check']:
         sum += 1
-    if not check_results['lowercase_error']:
+    if check_results['lowercase_check']:
         sum += 1
-    if not check_results['uppercase_error']:
+    if check_results['uppercase_check']:
         sum += 1
-    if not check_results['special_error']:
+    if check_results['special_check']:
         sum += 2
     return sum
 
@@ -76,12 +61,12 @@ if __name__ == '__main__':
     url_default = 'https://github.com/SBKubric/SecLists/raw/master/' \
                   'Passwords/10_million_password_list_top_1000000.txt'
     args = parse_args()
-    blacklist_location = args.blacklist
-    set_blacklist(args.blacklist, args.url, do_upload=args.url != url_default)
-    pwd = ''
+    print('Checking if there is a local copy of the blacklist_file...')
+    if not os.path.isfile(args.blacklist) or args.url != url_default:
+        print('Downloading the blacklist_file...')
+        upload_blacklist(args.blacklist, args.url)
     pwd = getpass.getpass('Enter your password:\n=> ')
     while pwd == '':
         print()
         pwd = getpass.getpass('The password can\'t have zero length!\n=> ')
-    res = get_password_strength(check_for_errors(pwd))
-    print('Password strength is %s out of 10.' % res)
+    print('Password strength is %s out of 10.' % get_password_strength(check_for_errors(pwd, args.blacklist)))
